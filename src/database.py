6d2fb6910eb59b003ff73dc170f0449e03a206f7
ituitis-otal18 <otal18@itu.edu.tr>
2022-10-01 00:30:00 +0300

    fixed env variables

--- src/database.py
+++ src/database.py
@@ -7,6 +7,10 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+print("DATABASE_URL: ", DATABASE_URL)
+print("SUPABASE_URL: ", SUPABASE_URL)
+print("SUPABASE_KEY: ", SUPABASE_KEY)
+
 
 db : Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
