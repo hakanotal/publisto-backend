760958b113756def3e3ba769cbd35324aa8d7c24
ituitis-otal18 <otal18@itu.edu.tr>
2022-12-07 22:08:29 +0300

    updated CORS policy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from mangum import Mangum
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from src.controller import UserController, ListController
 
 
 app = FastAPI(title='Publisto Documentation', root_path='/main')
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(UserController.router, prefix="/api/v1")
 app.include_router(ListController.router, prefix="/api/v1")
