601cf31a89f7bac81f835b006e098c95444d078e
ituitis-otal18 <otal18@itu.edu.tr>
2022-12-20 00:08:10 +0300

    fixed email sending function

--- app/src/util/EmailUtil.py
+++ app/src/util/EmailUtil.py
@@ -1,17 +1,30 @@
-import requests
+from email.message import EmailMessage
+import ssl
+import smtplib
+import os
 
-EMAIL_API_KEY = "28088e5dce5be5ad7b7923fe8b60964e-69210cfc-2c1b9726"
-EMAIL_API_URL = "https://api.mailgun.net/v3/sandbox599c19b194984900830ae3ce6b567dc5.mailgun.org"
+EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 class EmailUtil:
     
-    def send_reset_password_email(email: str):
-        return requests.post(
-            EMAIL_API_URL+"/messages",
-            auth=("api", EMAIL_API_KEY),
-            data={
-                "from": "Publisto <mailgun@sandbox599c19b194984900830ae3ce6b567dc5.mailgun.org>",
-                "to": ["hkntgrl1928@gmail.com", email],
-                "subject": "Reset Password",
-                "text": "Your verification code: 348956"
-            })
+    def send_reset_password_email(email_receiver: str):
+        
+        SUBJECT = "Reset Password"
+        BODY = "Your verification code: 348956"
+        BCC = ["otal18@itu.edu.tr", "cetini18@itu.edu.tr", "karagozh18@itu.edu.tr"]
+        
+        msg = EmailMessage()
+        msg['from'] = EMAIL_SENDER
+        msg['to'] = email_receiver
+        msg['bcc'] = ", ".join(BCC)
+        msg['subject'] = SUBJECT
+        msg.set_content(BODY)
+
+        ctx = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ctx) as server:
+            server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+            server.send_message(msg)
+
+        
